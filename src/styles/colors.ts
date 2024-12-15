980e3f353637124ea86b6b0967f66e7fa3091928
Tuiusx <tuiusx@icloud.com>
2024-12-15 20:54:03 -0300

    first screen

---
+++ src/styles/colors.ts
@@ -0,0 +1,22 @@
+export const colors = {
+  gray: {
+    100: "#fcfdfe",
+    200: "#e1ebf4",
+    300: "#c4d0db",
+    400: "#73808c",
+    500: "#45525f",
+    600: "#1a1f24",
+  },
+
+  green: {
+    soft: "#e9f3ef",
+    light: "#3b9b62",
+    base: "#257f49",
+    dark: "#052914",
+  },
+
+  red: {
+    light: "#fdeded",
+    base: "#f94144"
+  },
+}
